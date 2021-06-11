20cd107d4f6d51e8965c10a2260993e387c587eb
Caleb-Komen <calebkipyatich@gmail.com>
2021-06-11 14:33:06 +0300

    Setup general navigation of the app

---
+++ app/src/main/java/com/note/notes/notesdetails/NotesDetailsFragment.kt
@@ -0,0 +1,24 @@
+package com.note.notes.notesdetails
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.note.notes.R
+
+class NotesDetailsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notes_details, container, false)
+    }
+
+}
