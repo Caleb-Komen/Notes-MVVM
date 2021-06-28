dfe57f83406eafe0492d14c0bb37b0ebe9d816c2
Caleb-Komen <calebkipyatich@gmail.com>
2021-06-28 22:28:07 +0300

    Add dark theme

--- app/src/main/java/com/note/notes/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/note/notes/ui/settings/SettingsFragment.kt
@@ -1,11 +1,47 @@
 package com.note.notes.ui.settings
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
 import com.note.notes.R
+import timber.log.Timber
 
-class SettingsFragment: PreferenceFragmentCompat() {
+class SettingsFragment: PreferenceFragmentCompat(),
+    SharedPreferences.OnSharedPreferenceChangeListener {
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
     }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        when(key){
+            getString(R.string.switch_theme_key) -> {
+                val enable = sharedPreferences?.let {
+                    sharedPreferences.getBoolean(key, false)
+                }!!
+                enableDarkTheme(enable)
+            }
+        }
+    }
+
+    private fun enableDarkTheme(enable: Boolean) {
+        if (enable)
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        else
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+
 }
